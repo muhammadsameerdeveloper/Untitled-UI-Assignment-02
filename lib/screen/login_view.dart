76bef5cdc6f13a9f493d415339dd1e506100d1ca
muhammadsameerdeveloper <muhammadsameerdeveloper@gmail.com>
2026-09-24 20:26:22 +0500

    email & password textfield complete

--- lib/screen/login_view.dart
+++ lib/screen/login_view.dart
@@ -2,11 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:untitled/utils/app_colors.dart';
 import 'package:untitled/utils/size_config.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
   const LoginView({super.key});
 
   @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  bool isPasswordVisible = false;
+  @override
   Widget build(BuildContext context) {
+    TextEditingController emailController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
     SizeConfig.init(context);
     return Scaffold(
       backgroundColor: AppColors.whiteColor,
@@ -19,7 +27,56 @@ class LoginView extends StatelessWidget {
 
               width: SizeConfig.width * 0.45,
             ),
-            Text("Login to Your Account"),
+            Text(
+              "Login to Your Account",
+              style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 17),
+              child: TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey.shade100,
+                  prefixIcon: Icon(Icons.email),
+                  hintText: "andrew_ginsley@yourdomain.com",
+                  hintStyle: TextStyle(fontSize: 14),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 17),
+              child: TextField(
+                controller: passwordController,
+                obscureText: !isPasswordVisible,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey.shade100,
+                  prefixIcon: Icon(Icons.lock),
+                  hintText: "••••••••••••",
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isPasswordVisible = !isPasswordVisible;
+                      });
+                    },
+                    icon: Icon(
+                      isPasswordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
